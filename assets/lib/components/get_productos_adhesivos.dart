6221ed03abcfa93daea1f0a0a2072250901b0748
Julio Hernández <146073527+elrollox@users.noreply.github.com>
2024-03-14 00:26:30 -0600

    ok
    si

--- assets/lib/components/get_productos_adhesivos.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getProductosAdhesivos() async {
-  CollectionReference productosRef = FirebaseFirestore.instance.collection('adhesivos');
-  QuerySnapshot querySnapshot = await productosRef.get();
-
-  return querySnapshot.docs.map((doc) => doc as QueryDocumentSnapshot<Map<String, dynamic>>).toList();
-}
